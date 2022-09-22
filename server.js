53082e61ec3ffb1a6df600508899b95965af53a3
I3acon <poonawitch@gmail.com>
2022-09-22 21:22:25 +0700

    feat:mint&redeem token

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import config from "./config.json" assert { type: "json" };
 import Factory_abi from "./factory_abi.json" assert { type: "json" };
 import Voucher_abi from "./voucher_abi.json" assert { type: "json" };
+import Tokengated_abi from "./tokengated_abi.json" assert { type: "json" };
 import express from "express";
 import { ethers } from "ethers";
 
@@ -13,31 +14,70 @@ const factory = new ethers.Contract(
   wallet
 );
 
-app.get("/address", (req, res) => {
-  const result = factory.getVoucherAddress("Alice").then((address) => {
-    const Voucher = new ethers.Contract(address, Voucher_abi, wallet);
-    const name = Voucher.name().then((name) => {
-      res.send("Name : " + name);
+app.get("/address/:name", (req, res) => {
+  const result = factory
+    .getVoucherAddress(req.params["name"])
+    .then((address) => {
+      res.send(req.params["name"] + " Voucher Address : " + address);
     });
-  });
 });
 
-app.post("/mint", (req, res) => {
-  const result = factory.getVoucherAddress("Alice").then((address) => {
-    const Voucher = new ethers.Contract(address, Voucher_abi, wallet);
-    const mint = Voucher.mint(config.account.address);
-    console.log(mint);
-    res.send("Minted");
-  });
+app.post("/mint/:name/:address", (req, res) => {
+  const result = factory
+    .getVoucherAddress(req.params["name"])
+    .then((address) => {
+      const Voucher = new ethers.Contract(address, Voucher_abi, wallet);
+      async function mint() {
+        const tx = await Voucher.mint(req.params["address"]);
+        res.send("Transaction hash :" + tx.hash);
+      }
+      mint();
+    });
 });
 
-app.post("/redeem", (req, res) => {
-  const result = factory.getVoucherAddress("Alice").then((address) => {
-    const Voucher = new ethers.Contract(address, Voucher_abi, wallet);
-    const redeem = Voucher.redeem(config.account.address);
-    console.log(redeem);
-    res.send("Redeemed");
-  });
+app.post("/redeem/:name/:address", (req, res) => {
+  const result = factory
+    .getVoucherAddress(req.params["name"])
+    .then((address) => {
+      const Voucher = new ethers.Contract(address, Voucher_abi, wallet);
+      async function redeem() {
+        const tx = await Voucher.redeem(req.params["address"]);
+        res.send("Transaction hash :" + tx.hash);
+      }
+      redeem();
+    });
+});
+
+app.post("/mint/:name/:address/:tokenid", (req, res) => {
+  const result = factory
+    .getVoucherAddress(req.params["name"])
+    .then((address) => {
+      const Voucher = new ethers.Contract(address, Tokengated_abi, wallet);
+      async function mint() {
+        const tx = await Voucher.mint(
+          req.params["address"],
+          req.params["tokenid"]
+        );
+        res.send("Transaction hash :" + tx.hash);
+      }
+      mint();
+    });
+});
+
+app.post("/redeem/:name/:address/:tokenid", (req, res) => {
+  const result = factory
+    .getVoucherAddress(req.params["name"])
+    .then((address) => {
+      const Voucher = new ethers.Contract(address, Tokengated_abi, wallet);
+      async function redeem() {
+        const tx = await Voucher.redeem(
+          req.params["address"],
+          req.params["tokenid"]
+        );
+        res.send("Transaction hash :" + tx.hash);
+      }
+      redeem();
+    });
 });
 
 app.listen(3000, () => {
